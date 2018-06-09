a46798f9b2f122ed206fb4f2be5a47a359e5e92d
Shivle <sshivle@ptc.com>
2018-06-09 16:59:43 +0530

    ddd

---
+++ src/main/java/Day1/demo3/Calc.java
@@ -0,0 +1,24 @@
+package Day1.demo3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calc  {
+	public int add (int x, int y) {
+		return x+y;
+	}
+
+	public int divide(String a, String b) {
+		int i = Integer.parseInt(a);
+		int j = Integer.parseInt(b);
+		return i/j;
+	}}
+/*
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+ */
---
+++ src/test/java/Day1/demo3/CalculatorTest.java
@@ -0,0 +1,47 @@
+package Day1.demo3;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+Calc c1;
+	protected void setUp() throws Exception {
+		super.setUp();
+		c1 = new Calc();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testAdd() {
+		int i = c1.add(10, 20);
+		assertTrue("Add 10 and 20", i ==30);
+	}
+
+	public void testDivide() {
+		int i = c1.divide("100","20");
+		assertTrue("Divide 10 and 20", i ==5);
+	}
+
+	
+	public void testDivideZer0() {
+		try {
+		int i = c1.divide("1A0","0");
+		
+		}
+		catch (ArithmeticException e) {
+			assertTrue("NFE...", true);
+		}
+	}
+	
+	public void testDivideNonNumeric() {
+		try {
+		int i = c1.divide("1A0","20");
+		}
+		catch (NumberFormatException e) {
+			assertTrue("NFE...", true);
+		}
+	}
+	
+
+}
